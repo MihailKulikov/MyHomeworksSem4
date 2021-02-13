7026f65b4acd5360241857f86edc1fd63a1d26fc
Mikhail <kulikovmihail90@gmail.com>
2021-02-13 04:28:38 +0300

    Factorial

---
+++ hw1.1/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let rec factorial (n: UInt64): UInt64=
+    match n with
+    | 0UL -> 1UL
+    | _ -> n * factorial (n - 1UL)
+    
+[<EntryPoint>]
+let main _ =
+    match Console.ReadLine() |> UInt64.TryParse with
+    | true, n -> printf $"{factorial n}"
+    | _ -> printf "Input number should be a positive integer"
+    0
