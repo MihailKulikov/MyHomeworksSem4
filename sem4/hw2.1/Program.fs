55a372936ae9f21f165d3b3a68678b4ee089477e
Mikhail <kulikovmihail90@gmail.com>
2021-02-13 22:41:36 +0300

    It just works!

---
+++ sem4/hw2.1/Program.fs
@@ -0,0 +1,12 @@
+﻿open System.Numerics
+
+let fibonacciNumbers =
+    (BigInteger.Zero, BigInteger.One)
+    |> Seq.unfold (fun state ->
+        Some(fst state + snd state, (snd state, fst state + snd state)))
+
+
+[<EntryPoint>]
+let main _ =
+    Seq.iter (printf "%A ") fibonacciNumbers
+    0
