1ced0a5224fb8fd57f45b00b92caf8495bf82438
Mikhail <kulikovmihail90@gmail.com>
2021-02-14 01:38:24 +0300

    It just works!

---
+++ sem4/hw4.1/Program.fs
@@ -0,0 +1,15 @@
+﻿open System.Numerics
+open System
+
+let powersOfTwo n m =
+    let two = BigInteger.One + BigInteger.One
+    let rec loop element index =
+        if index = m + 1 then [] else element :: loop (element * two) (index + 1)
+    loop (BigInteger.Pow(two, n)) 0
+    
+[<EntryPoint>]
+let main _ =
+    printf "enter numbers n and m separated by a space.\n"
+    let input = Console.ReadLine().Split(' ') |> Seq.toList |> List.map Int32.Parse
+    powersOfTwo (List.head input) (List.last input) |> printf "%A"
+    0
