94b6cd1ead494736b8a1ebbd017f5ef384804263
MikhailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2021-02-14 04:17:40 +0300

    Style fix

--- sem4/hw1.3/Program.fs
+++ sem4/hw1.3/Program.fs
@@ -5,6 +5,6 @@ let reverse list =
 
 [<EntryPoint>]
 let main _ =
-    printf "Enter a list separated by a space. \n"
+    printf "Enter a list separated by a space.\n"
     Console.ReadLine().Split " " |> Seq.toList |> reverse |> printf "%A"
     0
