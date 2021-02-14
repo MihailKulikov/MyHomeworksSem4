6f65024655a56e0e5e80a59446196ff2b1a806b9
MikhailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2021-02-14 04:16:32 +0300

    It just works!

--- sem4/hw1.2/Program.fs
+++ sem4/hw1.2/Program.fs
@@ -1,12 +1,24 @@
-﻿open System.Numerics
+﻿open System
+open System.Numerics
 
 let fibonacciNumbers =
-    (BigInteger.Zero, BigInteger.One)
-    |> Seq.unfold (fun state ->
-        Some(fst state + snd state, (snd state, fst state + snd state)))
+    seq {
+        yield! seq { BigInteger.Zero; BigInteger.One }
+        yield! (BigInteger.Zero, BigInteger.One)
+        |> Seq.unfold (fun state ->
+            Some(fst state + snd state, (snd state, fst state + snd state)))}
 
+let getNthFibonacciNumber n =
+    if n <= 0 then None
+    else Seq.item (n - 1) fibonacciNumbers |> Some
 
 [<EntryPoint>]
 let main _ =
-    Seq.iter (printf "%A ") fibonacciNumbers
+    printf "Enter the number of the desired fibonacci number.\n"
+    match Console.ReadLine() |> Int32.TryParse with
+    | true, n ->
+        match getNthFibonacciNumber n with
+        | Some(result) -> printf "%A" result
+        | None -> printf "The input must be a positive integer."
+    | _ -> printf "The input must be a positive integer."
     0
