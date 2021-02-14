000fa1ddcbfbba92d4752e74f69be4bb01b0463c
MikhailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2021-02-14 16:56:03 +0300

    It just works!

--- sem4/hw1.4/Program.fs
+++ sem4/hw1.4/Program.fs
@@ -2,14 +2,26 @@
 open System
 
 let powersOfTwo n m =
+    if n < 0 || m < 0 then None else
     let two = BigInteger.One + BigInteger.One
-    let rec loop element index =
-        if index = m + 1 then [] else element :: loop (element * two) (index + 1)
-    loop (BigInteger.Pow(two, n)) 0
+    let startPow = BigInteger.Pow(two, n)
+    let rec loop element acc =
+        match acc with
+        | h :: _ when h = startPow -> acc
+        | _ -> loop (element / two) (element :: acc)
+    loop (BigInteger.Pow(two, n + m)) [] |> Some
     
 [<EntryPoint>]
 let main _ =
-    printf "enter numbers n and m separated by a space.\n"
-    let input = Console.ReadLine().Split(' ') |> Seq.toList |> List.map Int32.Parse
-    powersOfTwo (List.head input) (List.last input) |> printf "%A"
+    let parse (str: String) =
+        match Int32.TryParse str with
+        | true, number -> Some(number)
+        | _ -> None
+    printf "Enter numbers n and m separated by a space.\n"
+    match Console.ReadLine().Split ' ' |> Seq.toList |> List.map parse with
+    | [Some(head); Some(last)] ->
+        match powersOfTwo head last with
+        | Some(result) -> printf "%A" result
+        | None -> printf "n and m should be non-negative integers."
+    | _ -> printf "n and m should be non-negative integers."
     0
