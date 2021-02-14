95378138f0b636fe9bc44aea4959729d1a6d6599
MikhailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2021-02-14 03:26:47 +0300

    Works

--- sem4/hw1.1/Program.fs
+++ sem4/hw1.1/Program.fs
@@ -1,26 +1,19 @@
 ﻿open System
 open System.Numerics
 
-let factorial (n: BigInteger): BigInteger =
-    if n < BigInteger.Zero then
-        raise (ArgumentException "The input must be a non-negative integer")
-    let rec loop (n: BigInteger) (acc: BigInteger): BigInteger =
-        if n = BigInteger.Zero then
-            acc
-        else
-            loop (acc * n) (n - BigInteger.One)
-    loop n BigInteger.One
-    
-let tryFactorial (n: BigInteger) =
-    try
-        Some(factorial n)
-    with
-    | :? ArgumentException ->  None
+let factorial n =
+    let rec loop n acc =
+        if n = BigInteger.Zero then acc
+        else loop (n - BigInteger.One) (acc * n)
+    if n < BigInteger.Zero then None
+    else Some (loop n BigInteger.One)
     
 [<EntryPoint>]
 let main _ =
+    printf "Enter the number for which you want to calculate the factorial. \n"
     match Console.ReadLine() |> BigInteger.TryParse with
-    | true, n -> match tryFactorial n with
+    | true, n ->
+        match factorial n with
         | Some(result) -> printf $"{result}"
         | None -> printf "The input must be a non-negative integer."
     | _ -> printf "The input must be a non-negative integer."
