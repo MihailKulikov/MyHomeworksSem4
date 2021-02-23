c68b071c81ed100bf6dfebafc9f1811aaf79692d
MikhailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2021-02-23 03:11:26 +0300

    It just works!

---
+++ sem4/hw2.1/hw2.1/Program.fs
@@ -0,0 +1,18 @@
+﻿module Program
+    let evenNumberCountMapping list =
+        list
+        |> List.map(fun x -> if x % 2 = 0 then 1 else 0) 
+        |> List.sum
+    
+    let evenNumberCountFiltering list =
+        list
+        |> List.filter(fun x -> if x % 2 = 0 then true else false)
+        |> List.length
+    
+    let evenNumberCountFolding list =
+        list
+        |> List.fold (fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0
+    
+    [<EntryPoint>]
+    let main _ =
+        0
