6cf6d6012da166ad6e736b78fc3cb3d58a4cbd0d
MikhailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2021-02-24 19:27:17 +0300

    Style fix

--- sem4/hw2.3/hw2.3/Program.fs
+++ sem4/hw2.3/hw2.3/Program.fs
@@ -1,9 +1,7 @@
 ﻿module Program
 
-/// <summary>
-/// Represents the abstract syntax tree of an expression in a simple programming language that supports addition,
-/// subtraction, multiplication and division of numbers and variables.
-/// </summary>
+/// <summary>Represents the abstract syntax tree of an expression in a simple programming language that supports addition,
+/// subtraction, multiplication and division of numbers and variables.</summary>
 type Expression =
     | Number of int
     | Add of Expression * Expression
