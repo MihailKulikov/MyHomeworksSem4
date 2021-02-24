487359de85e36cf56fb0ad6f059e6468cca71a5a
MikhailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2021-02-24 19:01:54 +0300

    It just works with tests

---
+++ sem4/hw2.4/Tests/Tests.fs
@@ -0,0 +1,34 @@
+module Tests
+
+open NUnit.Framework
+open Program
+open FsUnit
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<TestCase(3, ExpectedResult = true)>]
+[<TestCase(97, ExpectedResult = true)>]
+[<TestCase(2, ExpectedResult = true)>]
+[<TestCase(2147483647, ExpectedResult = true)>]
+[<Test>]
+let ``isPrime on primes numbers should return true`` (testCase: int) =
+    isPrime <| bigint testCase
+
+[<TestCase(4, ExpectedResult = false)>]
+[<TestCase(341, ExpectedResult = false)>]
+[<TestCase(221, ExpectedResult = false)>]
+[<TestCase(2047, ExpectedResult = false)>]
+let ``isPrime on composite numbers should return false`` (testCase: int) =
+    isPrime <| bigint testCase
+    
+[<TestCase(0, ExpectedResult = false)>]
+[<TestCase(1, ExpectedResult = false)>]
+[<TestCase(-1, ExpectedResult = false)>]
+[<TestCase(-101, ExpectedResult = false)>]
+let ``interesting cases of not prime numbers`` (testCase: int) =
+    isPrime <| bigint testCase
+ 
+let ``first 10 elements of prime sequence should be correct`` =
+    primes |> Seq.take 10 |> Seq.toList |> should equal [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
---
+++ sem4/hw2.4/hw2.4/Program.fs
@@ -0,0 +1,17 @@
+﻿module Program
+
+let isPrime (number: bigint) =
+    if number <= bigint.One then false
+    else
+        let upperBound = number |> float |> sqrt |> bigint
+        seq { bigint(2)..upperBound }
+        |> Seq.exists(fun x -> number % x = bigint.Zero)
+        |> not
+    
+let primes =
+    Seq.initInfinite(fun x -> bigint(x) + bigint(2))
+    |> Seq.filter(isPrime)
+
+[<EntryPoint>]
+let main _ =
+    0
