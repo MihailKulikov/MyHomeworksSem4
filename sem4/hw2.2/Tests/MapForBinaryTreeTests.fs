d47ad12d5c4d9b5ad67683d5d40e9aa4d5586b34
MikhailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2021-02-25 16:06:43 +0300

    Style fixes

--- sem4/hw2.2/Tests/MapForBinaryTreeTests.fs
+++ sem4/hw2.2/Tests/MapForBinaryTreeTests.fs
@@ -7,21 +7,17 @@ open Program
 type Tests () =
     static member TestCaseList = [|
         Node(42, Node(1, Empty, Empty), Node(3, Empty, Empty)),
-        (fun (x: int) -> x.ToString()),
+        (fun (x: int) -> string x),
         Node("42", Node("1", Empty, Empty), Node("3", Empty, Empty))
         
         Empty,
-        (fun (x: int) -> x.ToString()),
+        (fun (x: int) -> string x),
         Empty
         
         Node(42, Node(1, Empty, Empty), Node(3, Empty, Empty)),
-        (fun (x: int) -> (x + 5).ToString()),
+        (fun (x: int) -> string (x + 5)),
         Node("47", Node("6", Empty, Empty), Node("8", Empty, Empty))
     |]
-    
-    [<SetUp>]
-    member this.Setup () =
-        ()
         
     [<TestCaseSource(nameof(Tests.TestCaseList))>]
     [<Test>]
