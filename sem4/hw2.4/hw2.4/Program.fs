a96d02eeeee449337d1e9fb247de1f3de824db9d
MihailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2021-02-25 16:15:13 +0300

    Primes (#11)
    * It just works with tests
    
    * XML documentation was added
    
    * appveyor was updated
    
    * Style fixes

---
+++ sem4/hw2.4/hw2.4/Program.fs
@@ -0,0 +1,21 @@
+﻿module Program
+
+/// <summary>Checks that the specified number is prime.</summary>
+/// <param name="number">The specified number.</param>
+/// <results>true if the specified number is prime; otherwise false.</results>
+let isPrime (number: bigint) =
+    if number <= bigint.One then false
+    else
+        let upperBound = number |> float |> sqrt |> bigint
+        seq { bigint(2)..upperBound }
+        |> Seq.exists (fun x -> number % x = bigint.Zero)
+        |> not
+
+/// Infinite sequence of prime numbers.
+let primes =
+    Seq.initInfinite (fun x -> bigint(x) + bigint(2))
+    |> Seq.filter isPrime
+
+[<EntryPoint>]
+let main _ =
+    0
