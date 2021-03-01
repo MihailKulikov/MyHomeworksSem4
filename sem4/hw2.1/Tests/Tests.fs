640a6c0ecb641f04a11c867e7607a5837b13616a
MihailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2021-03-01 19:38:23 +0300

    MapFoldFilter (#10)
    * It just works!
    
    * appveyor was updated
    
    * XML documentation was added.
    
    * Useless semicolons were removed
    
    * Style fixes

---
+++ sem4/hw2.1/Tests/Tests.fs
@@ -0,0 +1,46 @@
+module Tests
+
+open NUnit.Framework
+open FsUnit
+open FsCheck
+open Program
+open System
+
+type ListProperties =
+    static member ``filtering implementation should be equivalent to folding realisation`` list =
+            (evenNumberCountFiltering list) = (evenNumberCountFolding list)
+    static member ``filtering implementation should be equivalent to mapping realisation`` list =
+            (evenNumberCountFiltering list) = (evenNumberCountMapping list)
+    static member ``folding implementation should be equivalent to mapping realisation`` list =
+            (evenNumberCountFolding list) = (evenNumberCountMapping list)
+
+[<TestFixture(nameof evenNumberCountMapping)>]
+[<TestFixture(nameof evenNumberCountFolding)>]
+[<TestFixture(nameof evenNumberCountFiltering)>]
+type Tests (realisation) =
+    member this.evenCount =
+        match realisation with
+        | nameof evenNumberCountMapping -> evenNumberCountMapping
+        | nameof evenNumberCountFolding -> evenNumberCountFolding
+        | nameof evenNumberCountFiltering -> evenNumberCountFiltering
+        | _ -> raise (ArgumentException("Invalid test fixture parameter."))
+        
+    static member TestCaseList = [|
+        [], 0
+        [42; 54], 2 
+        [-2; -1], 1
+        [0; -0], 2
+        [1; 100], 1
+        [1000000; 1000000001], 1
+        [Int32.MaxValue; Int32.MinValue], 1
+    |]
+ 
+    [<Test>]
+    member this.``Functions should be equivalent`` () =
+        Check.QuickThrowOnFailureAll<ListProperties>()
+    
+    [<TestCaseSource(nameof(Tests.TestCaseList))>]
+    [<Test>]
+    member this.``Mapping realisation should work on various inputs`` testCase =
+        let data, expectedResult = testCase
+        this.evenCount data |> should equal expectedResult
---
+++ sem4/hw2.1/hw2.1/Program.fs
@@ -0,0 +1,34 @@
+﻿module Program
+    
+    /// <summary>
+    /// Counts the number of even numbers in a list using the map function.
+    /// </summary>
+    /// <param name="list">List in which you need to count the number of even numbers.</param>
+    /// <returns>The number of even numbers in the list.</returns>
+    let evenNumberCountMapping list =
+        list
+        |> List.map (fun x -> if x % 2 = 0 then 1 else 0) 
+        |> List.sum
+    
+    /// <summary>
+    /// Counts the number of even numbers in a list using the filter function.
+    /// </summary>
+    /// <param name="list">List in which you need to count the number of even numbers.</param>
+    /// <returns>The number of even numbers in the list.</returns>
+    let evenNumberCountFiltering list =
+        list
+        |> List.filter (fun x -> x % 2 = 0)
+        |> List.length
+    
+    /// <summary>
+    /// Counts the number of even numbers in a list using the fold function.
+    /// </summary>
+    /// <param name="list">List in which you need to count the number of even numbers.</param>
+    /// <returns>The number of even numbers in the list.</returns>
+    let evenNumberCountFolding list =
+        list
+        |> List.fold (fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0
+    
+    [<EntryPoint>]
+    let main _ =
+        0
