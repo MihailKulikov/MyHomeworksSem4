2fa47f96ccd62e7a1b5f6f7a4f3fd24385178a9b
MikhailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2021-03-18 20:29:37 +0300

    Well, it's working

--- sem4/hw4.1/CheckBracketSequenceCorrectness/CheckBracketSequenceCorrectness/Program.fs
+++ sem4/hw4.1/CheckBracketSequenceCorrectness/CheckBracketSequenceCorrectness/Program.fs
@@ -1,13 +1,39 @@
-﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
-open System
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+﻿let check (bracketSequence: string) bracketsPairs =
+    let containsElement pair bracket =
+        fst pair = bracket || snd pair = bracket
+    let bracketToPair bracket =
+        bracketsPairs |> List.find (fun pair -> containsElement pair bracket) 
+    let isLeft bracket =
+        (bracket |> bracketToPair |> fst) = bracket 
+    let rec loop (bracketSequence: string) bracketPairToCountMap =
+        if bracketSequence.Length = 0
+        then
+            bracketPairToCountMap
+        else
+            let currentBracket = bracketSequence.[0]
+            let diff = if currentBracket |> isLeft then 1 else -1
+            bracketPairToCountMap
+            |> Map.change (bracketToPair currentBracket) (fun oldValue ->
+                match oldValue with
+                | Some(count) when count >= 0 -> Some (count + diff)
+                | Some(count) -> Some (count)
+                | None -> None)
+            |> loop bracketSequence.[1..]
+    if bracketSequence
+       |> Seq.forall (fun bracket -> bracketsPairs |> List.exists (fun pair -> containsElement pair bracket)) |> not
+    then
+        None
+    else
+        bracketsPairs
+        |> List.map (fun pair -> (pair, 0))
+        |> Map.ofList
+        |> loop bracketSequence
+        |> Map.forall (fun _ count -> count = 0)
+        |> Some
 
 [<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+let main _ =
+    match check "((())){}" ['(', ')'; '}', '{' ] with
+    | Some(result) -> printfn "%A" result
+    | None -> printfn "Something goes wrong."
+    0
