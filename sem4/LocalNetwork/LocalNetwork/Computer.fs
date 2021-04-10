c7141519ac929deeaabcadd9f31fc1dfe093ea83
MikhailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2021-04-10 21:40:40 +0300

    All should work

---
+++ sem4/LocalNetwork/LocalNetwork/Computer.fs
@@ -0,0 +1,14 @@
+﻿namespace LocalNetwork
+
+/// Possible types of operating systems in the local network. 
+type Os =
+    | Windows = 0
+    | Linux = 1
+    | MacOs = 2
+
+/// Represents computer in the local network.
+type Computer =
+    abstract member Os: Os
+    abstract member TryToGetInfected: unit -> unit
+    abstract member IsInfected: bool
+    abstract member ProbabilityOfInfection: float 
---
+++ sem4/LocalNetwork/LocalNetwork/LocalNetwork.fs
@@ -0,0 +1,25 @@
+﻿namespace LocalNetwork
+
+open System.Collections.Generic
+
+/// Represents the local network.
+type LocalNetwork(adjacencyMap: IDictionary<Computer, Computer list>) =
+    let computers = Seq.toList adjacencyMap.Keys
+
+    let canThisComputerBeInfected (computer: Computer) =
+        computer.IsInfected = false && computer.ProbabilityOfInfection > 0.0
+
+    let canThisComputerInfect (computer: Computer) =
+        computer.IsInfected = true
+        && List.exists canThisComputerBeInfected adjacencyMap.[computer]
+        
+    let tryInfectNeighboursOf (computer: Computer) =
+        List.iter (fun (neighbour: Computer) -> neighbour.TryToGetInfected()) adjacencyMap.[computer]
+    
+    /// Returns true if the state of the local network can change; false otherwise.
+    member this.IsEndOfSimulation () = List.exists canThisComputerInfect computers
+
+    /// Makes step in the simulation.
+    member this.MakeStep () =
+        List.iter tryInfectNeighboursOf computers
+        List.map (fun (computer: Computer) -> computer.IsInfected) computers
