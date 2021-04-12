a1ecbfb2df50048ebce190501b84561f5f9328a7
MikhailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2021-04-12 23:11:14 +0300

    All works

---
+++ sem4/CalculateFlow/CalculateFlow/Program.fs
@@ -0,0 +1,21 @@
+﻿open System  
+
+type CalculateBuilder() =
+    member this.Bind(x, f) =
+        match Int32.TryParse(x: string) with
+        | true, number -> f number
+        | _ -> None
+    member this.Return(x) =
+        Some x
+
+[<EntryPoint>]
+let main _ =
+    let calculate = CalculateBuilder()
+    let result = calculate {
+        let! x = "1"
+        let! y = "3"
+        let z = x + y
+        return z
+    }
+    printf $"%A{result}"
+    0
