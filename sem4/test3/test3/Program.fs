cddd024713bb918f1077397cb876252162d3019f
MikhailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2021-04-23 18:49:43 +0300

    refactoring and xml documentation

--- sem4/test3/test3/Program.fs
+++ sem4/test3/test3/Program.fs
@@ -1,49 +1,68 @@
 ﻿module PriorityQueue  
 
+/// <summary>
+/// Represents priority queue with enqueue and dequeue methods
+/// </summary>
 type PriorityQueue<'a> (capacity, comparator) =
     let pq : 'a array = Array.zeroCreate (capacity + 1)
     let mutable count = 0
-    let Less i j = comparator pq.[i] pq.[j] < 0
+    let isLess i j = comparator pq.[i] pq.[j] < 0
 
-    let Exchange i j =
+    let exchange i j =
         let temp = pq.[i]
         pq.[i] <- pq.[j]
         pq.[j] <- temp
         i
         
-    let rec Swim k =
-        match (k > 1 && Less (k/2) k) with
-        | true -> Exchange (k/2) k |> Swim 
+    let rec swim k =
+        match (k > 1 && isLess (k/2) k) with
+        | true -> exchange (k/2) k |> swim 
         | false -> ()
 
-    let rec Sink k =  
+    let rec sink k =  
         match 2*k <= count, 2*k with
-        | true, j when j < count && Less j (j + 1) ->
-            match Less k (j+1), j+1 with  
+        | true, j when j < count && isLess j (j + 1) ->
+            match isLess k (j+1), j+1 with  
             | true, x ->
-                Exchange k x |> ignore
-                Sink x
+                exchange k x |> ignore
+                sink x
             | false, _ -> ()                                     
         | true, j ->
-            Exchange k j |> ignore
-            Sink j
+            exchange k j |> ignore
+            sink j
         | false, _ -> ()  
 
+    /// <summary>
+    /// Checks if the priority queue is empty.
+    /// </summary>
+    /// <returns>True if the priority queue is empty, and false if not.</returns>
     member x.IsEmpty with get() = count = 0
-    member x.Size with get() = count
+    
+    /// <summary>
+    /// Gets the number of elements actually contained in the priority queue.
+    /// </summary>
+    member x.Length with get() = count
 
-    member x.Insert (item:'a) =
+    /// <summary>
+    /// Adds an object to the end of the Queue&lt;T&gt;.
+    /// </summary>
+    /// <param name="item">The object to add to the priority queue.</param>
+    member x.Enqueue (item:'a) =
         count <- count + 1
         pq.[count] <- item
-        Swim count
+        swim count
 
-    member x.Pop() =
+    /// <summary>
+    /// Removes and returns the object at the beginning of the priority queue.
+    /// </summary>
+    /// <returns>The object that is removed from the beginning of the priority queue.</returns>
+    member x.Dequeue() =
         if count = 0 then invalidOp "Queue is empty."
         let max = pq.[1]
-        Exchange 1 count |> ignore
+        exchange 1 count |> ignore
         count <- count - 1
         pq.[count+1] <- Unchecked.defaultof<'a>
-        Sink 1
+        sink 1
         max
         
 let main _ =
