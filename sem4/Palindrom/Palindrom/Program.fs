551e025aa1486a2cc53c757668d97f97adcb7b01
MikhailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2021-04-23 17:56:22 +0300

    all works

---
+++ sem4/Palindrom/Palindrom/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+
+let combinations size set =
+    let rec loop acc size set = seq {
+      match size, set with
+      | n, x::xs ->
+          if n > 0 then yield! loop (x::acc) (n - 1) xs
+          if n >= 0 then yield! loop acc n xs
+      | 0, [] -> yield acc
+      | _, [] -> () }
+    loop [] size set
+    
+let isPalindrome number =
+    let stringRepresentation = number.ToString().ToCharArray()
+    stringRepresentation = Array.rev stringRepresentation
+    
+let findPairForMaxPalindrome() =
+    let combinationsWithRepetition = Seq.init 900 (fun i -> [i + 100; i + 100])
+                                     |> Seq.append <| combinations 2 [100..999]
+    combinationsWithRepetition |> Seq.map (fun pair -> pair.[0] * pair.[1]) |> Seq.filter isPalindrome |> Seq.max
+
+[<EntryPoint>]
+let main _ =
+    printf $"%A{findPairForMaxPalindrome()}"
+    0
